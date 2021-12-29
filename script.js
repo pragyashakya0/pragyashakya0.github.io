97a521dc1c11ad10ae98945588bcadbfd9d576d5
Pragya Shakya <pragyashakya001@gmail.com>
2021-12-29 20:18:23 +0545

    hover in gameMenu

--- script.js
+++ script.js
@@ -1,38 +1,57 @@
 var slideIndex = 0;
 showSlides();
 function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("slides");
-  var dots = document.getElementsByClassName("dotItem");
+  let i;
+  let slides = document.getElementsByClassName("slides");
+  let dots = document.getElementsByClassName("dotItem");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
   }
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
+  if (slideIndex > slides.length) 
+  {
+    slideIndex = 1;
+  }    
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000); 
+  setTimeout(showSlides, 3000); 
 }
 
 
 var sIndex = 0;
 slideShow();
 function slideShow() {
-  var i;
-  var slides = document.getElementsByClassName("testslides");
-  var dots = document.getElementsByClassName("dotItem");
+  let i;
+  let slides = document.getElementsByClassName("testslides");
+  var dot = document.getElementsByClassName("dotItems");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
   }
   sIndex++;
   if (sIndex > slides.length) {sIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+  for (i = 0; i < dot.length; i++) {
+    dot[i].className = dot[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(slideShow, 2000); 
+  dot[slideIndex-1].className += " active";
+  setTimeout(slideShow, 3000); 
 }
+
+var menu = document.getElementById("gameMenu");
+var btn = menu.getElementsByClassName("menuItem");
+for(let i = 0; i<btn.length; i++){
+  btn[i].addEventListener("click",function(){
+  var current = menu.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  }); 
+}
+
+
+
+
+let games = document.getElementsByClassName("gameCard");
+console.log(games)
