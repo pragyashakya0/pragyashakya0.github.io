e17e989e8a827b3aec5a2580ad4cf501cf226ae9
Pragya Shakya <pragyashakya001@gmail.com>
2021-12-31 12:09:02 +0545

    create games modal and newsletter button

--- script.js
+++ script.js
@@ -51,7 +51,187 @@ for(let i = 0; i<btn.length; i++){
 }
 
 
+function myMenu(n,type){
+  let games = document.getElementsByClassName("gameCard");
+
+  for (let i = 0; i < games.length; i++) {
+    let gameType = games[i].getAttribute("data-game-type");
+
+    if (gameType === type) {
+      games[i].style.display = "block";
+    } 
+    else 
+    {
+      games[i].style.display = "none";
+    }
+  }
+}
 
 
-let games = document.getElementsByClassName("gameCard");
-console.log(games)
+
+
+function gameBtn(id){
+  let modal =document.querySelectorAll(".gamePopup");
+  let closeBtn = document.getElementsByClassName("close");
+  console.log(closeBtn)
+  console.log(modal)
+  for(let i = 0;  i<modal.length; i++){
+    let modalId = modal[i].id;
+    if(id === modalId){
+    modal[i].style.display ="block";
+
+    closeBtn.onclick = function(){
+    modal[i].style.display ="none";
+}
+
+
+
+    window.onclick = function(event) {
+    if (event.target == modal[i]) {
+    modal[i].style.display = "none";
+  }
+}
+}
+
+ 
+
+    
+
+
+
+
+}
+}
+
+
+// var modal = document.querySelector(".gamePopup");
+// var btn = document.getElementById("marioCard");
+// var closeBtn = document.getElementsByClassName("close")[0];
+
+// btn.onclick = function(){
+//   modal.style.display="block";
+// }
+
+// closeBtn.onclick = function(){
+//   modal.style.display ="none";
+// }
+
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal[i].style.display = "none";
+//   }
+// }
+
+
+
+// const modal = document.querySelector('#gamePopup');
+// const popup =document.getElementById("gamePopup")
+// const gameCard = document.querySelector('#marioCard');
+// const closeBtn = document.querySelector('.close');
+
+
+// gameCard.addEventListener('click', openModal);
+// closeBtn.addEventListener('click', closeModal);
+// window.addEventListener('click', outsideClick);
+
+
+
+// function openModal() {
+//   // window.addEventListener('click', outsideClick);
+//   // console.log("run")
+//   modal.style.display = 'block';
+// }
+
+// function closeModal() {
+//   modal.style.display = 'none';
+//   // window.removeEventListener('click', outsideClick);
+// }
+
+
+// function outsideClick(e) {
+//     const popup = document.getElementById("gamePopup");
+//     let targetelement = e.target;
+//     console.log(targetelement)
+
+//     do{
+//       if(targetelement === popup ){
+//           modal.style.display = 'none';
+//       }
+//       targetelement = targetelement.parentnode;
+//     }
+//     while(targetelement);
+//       modal.style.display = 'none';
+
+//   }
+
+  // console.log(e.target, 'logg',popup )
+  // var pop = popup.contains(e.target);
+
+  // console.log(pop)
+  // if(!pop){
+  //   modal.style.display = 'none';
+  // }
+
+  //  window.removeEventListener('click', outsideClick);
+
+//   if (e.target == gamePopup ) {
+//     modal.style.display = 'none';
+//   }
+//}
+// && modal.style.display === "block"
+
+
+
+
+
+
+
+
+
+
+
+function subscribe()
+{
+  document.getElementById("subscribe").style.display = "block";
+  document.getElementById("form").style.display = "none";
+}
+
+
+const gameList = 
+[
+     {
+        "Image": "/img/supermariobros.png",
+        "name": "Super Mario"
+    },
+
+    {
+        "Image": "/img/excitebike.png",
+        "name": "Excite Bike"
+    },
+
+    { 
+        "Image": "/img/pacman.png",
+        "name": "Pacman"
+    },
+  
+    {
+        "Image": "/img/castlevania.png",
+        "name": "Castlvania"
+    },
+  
+    {
+        "Image": "/img/image 5.png",
+        "name": "Duck Hunt"
+    },
+
+    {
+        "Image": "/img/image 4.png",
+        "name": "The Legend of Zelda"
+    },
+
+    {
+        "Image": "/img/image 3.png",
+        "name": "Mega Man"
+    }
+];
+
